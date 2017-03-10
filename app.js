16fafafbca675a58c0f80562cc93b8af51eaf796
sarathi0333 <sarath.kct@gmail.com>
2017-03-10 22:36:04 +0530

    git commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT
+var PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send("hello world")
