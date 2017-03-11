0437aa438e99aefc10a123b47284332b9cd5fa78
sarathi0333 <sarath.kct@gmail.com>
2017-03-11 13:08:23 +0530

    git commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
-var app = express();
+var request = require('request');
 var bodyParser = require('body-parser');
+var app = express();
 var PORT = process.env.PORT || 3000;
-var access_token = 'EAAFdYj6cQ9IBALbWR4Vp7msZCrXNZBWgUsK1qkZBww08CWd3b1aRyILCtL43516mtJZAyW7q4iedJd89UrT0HEZAQQDnd4vWJUZCWrB9rPr7KD8iplTJbKIRXbsQZCxmDMQZAZAq1IWdvKWrdjCSMtmE4lLmYxcvWr9wq5pleTCiCzQZDZD';
+var PAGE_ACCESS_TOKEN = 'EAAFdYj6cQ9IBAIwmhdw73Fx1DZAKEsJpd2144Wg4sk5PlzKGqkH7Pf1vi1zLsbH9CWwaxUFL0bqYAoFdhs0i28N9SpZA3dqH2kk6nbJgi87lu0dgNj23bxOXHtPeclXu3bW5XdQmkTFRYTcj5ZCZAhVyyyjY9gZCpNOVXCZB2AXgZDZD';
 
 app.use(bodyParser.json());
 
@@ -14,7 +15,7 @@ app.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'abc123') {
       res.send(req.query['hub.challenge']);
     } else {
-      res.send('Error, wrong validation token');    
+      res.send('Error, wrong validation token');   
     }
   });
 
@@ -49,8 +50,74 @@ app.post('/webhook', function (req, res) {
 });
   
 function receivedMessage(event) {
-  // Putting a stub for now, we'll expand it in the following steps
-  console.log("Message data: ", event.message);
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendGenericMessage(recipientId, messageText) {
+  // To be expanded in later sections
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
 }
 
 // curl -X POST "https://graph.facebook.com/v2.8/me/subscribed_apps?access_token=access_token"
